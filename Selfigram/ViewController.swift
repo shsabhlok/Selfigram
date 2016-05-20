3fc846e3b08da9f95dfd157e58f2acd2daed0ef1
Shashank <shashanksabhlok95@gmail.com>
2016-05-19 22:01:31 -0400

    Initial Commit

---
+++ Selfigram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Selfigram
+//
+//  Created by Shashank Sabhlok on 2016-05-19.
+//  Copyright © 2016 Shashank Sabhlok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
